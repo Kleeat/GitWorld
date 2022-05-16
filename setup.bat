0244132045231a0283f2ca31cebed4107bd39ef5
Levente Szénási <senasilevixd@gmail.com>
2022-05-16 12:25:31 +0200

    Update

--- setup.bat
+++ setup.bat
@@ -1,3 +1,4 @@
 @echo off
+start C:\Users\senas\AppData\Roaming\.minecraft\saves\GitWorld\pull.sh
 /wait "D:\Games\runtime\java-runtime-beta\windows-x64\java-runtime-beta\bin\javaw.exe"
 start C:\Users\senas\AppData\Roaming\.minecraft\saves\GitWorld\commit.sh
